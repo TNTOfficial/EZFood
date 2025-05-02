8b11efbb507ca1fa87ab62cecbad71a061f1760e
Ram Saini <sainiram226@gmail.com>
2025-05-02 17:25:38 +0530

    enviroment localhost add

--- EZFood.admin/src/environments/environment.ts
+++ EZFood.admin/src/environments/environment.ts
@@ -4,4 +4,8 @@ export const environment = {
   assetsUrl: 'http://143.198.101.124:5000/',
   // apiUrl: 'https://localhost:7184/api',
   // assetsUrl: 'https://localhost:7184/',
+  
+  
+  // apiUrl: 'http://localhost:5188/api',
+  // assetsUrl: 'http://localhost:5188/',
 };
