1bb0710233db0982b0f21774ccfbecc0fb80a33b
Gurwinder Singh <gurwinder.gwebs@gmail.com>
2025-05-07 19:41:30 +0530

    updated code for onboarding action

---
+++ EZFood.Domain/Entities/Models/OnboardingAction.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace EZFood.Domain.Entities.Models;
+
+public class OnboardingAction
+{
+    [Key]
+    public Guid Id { get; set; }
+    [Required]
+    [MaxLength(30, ErrorMessage = "Maximum length for Name is 60 characters.")]
+    public required string Name { get; set; }
+    [MaxLength(200, ErrorMessage = "Maximum length for Description is 200 characters.")]
+    public string? Description { get; set; }
+    public bool Status { get; set; } = true;
+    public List<TruckDetail> TruckDetails { get; } = [];
+
+}
